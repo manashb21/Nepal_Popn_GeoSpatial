b80b0d90d689a29c9fc76cd77e69f75c945567e9
Manash .B <manash.bhele21@gmail.com>
2021-12-16 16:31:53 +0545

    Analysis Done with Jpeg File

--- nep_popnDensity.py
+++ nep_popnDensity.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import geopandas as gpd 
+import matplotlib.pyplot as plt
 
 # data = pd.read_html("https://www.citypopulation.de/php/nepal-mun-admin.php")
 
@@ -31,4 +32,55 @@ for index, row in popn_data.iterrows():
         
 popn_data = popn_data[['Districts2','Population']]
 
-popn_data.rename(columns = {'Districts2': 'Districts'}, inplace = True)
+#renaming the column
+popn_data.rename(columns = {'Districts2': 'District'}, inplace = True)
+
+#reading data from the shapefile 
+nep_districts = gpd.read_file(r'NPL_adm\NPL_adm3.shp')
+
+nep_districts = nep_districts[['NAME_3','geometry']]
+
+nep_districts.rename(columns ={'NAME_3':'District'}, inplace = True)
+
+#reprojecting to projected coordinate system
+nep_districts.to_crs(epsg = 32645, inplace = True)
+
+
+popn_data.replace('Sindhupalchowk','Sindhupalchok', inplace =True)
+popn_data.replace('Chitwan','Chitawan', inplace =True)
+popn_data.replace('Tehrathum','Terhathum', inplace =True)
+popn_data.replace('Dang Deukhuri','Dang', inplace =True)
+popn_data.replace('Tanahun','Tanahu', inplace =True)
+popn_data.replace('Kapilvastu','Kapilbastu', inplace =True)
+
+for index, row in nep_districts['District'].iteritems():
+    if row in popn_data['District'].tolist():
+        pass
+    else:
+        print('The value ', row, ' is NOT in popn_data list.')
+        
+
+nep_districts['Area'] = nep_districts.area/1000000
+
+#Do an attributes join
+nep_districts = nep_districts.merge(popn_data, on = 'District')
+
+        
+#calculation of population density
+nep_districts['PopnDensity (people/sqkm)'] = nep_districts['Population']/nep_districts['Area']
+
+#plotting the density data 
+nep_districts.plot(cmap = 'jet', column = 'PopnDensity (people/sqkm)', legend = True, figsize = (10,10))
+plt.savefig('Nepal_popnDensity.jpg')
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
